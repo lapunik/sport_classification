03f6f186f4d99417e8feb5be53c670b69d83fa6b
Vojtech Lapunik <vojtechlapunik@gmail.com>
2024-12-05 11:14:27 +0100

    requirements update

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 from train import create_model
 from test import use_model
 
-available_models = ["bert",
-                    "native_bayes", # tfidf: 0.85, count: 0.97, hashing: 0.70
+available_models = ["bert", 
+                    "native_bayes", # tfidf: 0.85, count: 0.97, hashing: 0.70, 
                     "logistic_regression", # tfidf: 0.96, count: 0.98, hashing: 0.96
-                    "svm"]
+                    "svm"] # tfidf: 0.98, count: 0.97, hashing: 0.98
 
 available_vectorizers = ["count", 
                          "tfidf", 
-                         "hashing",
-                         "sentence_transformer"]
+                         "hashing",]
 
 available_data = ["sportoclanky",
                   "first100",
                   "first1k",
                   "first10"]
 
-model = "logistic_regression"
-vectorizer = "sentence_transformer"
-data = "sportoclanky"
-retrain = True
+# model = "bert"
+# vectorizer = "hashing"
+# data = "first10"skript.sh
+# retrain = True
+model = "svm"
+vectorizer = "hashing"
+data = "first10"
+retrain = False
 
-title = "Zemřel wimbledonský vítěz. Fraser byl v tenisu pojmem"
-perex = "Ve věku 91 let zemřel bývalý australský tenista Neale Fraser. O úmrtí trojnásobného grandslamového vítěze ve dvouhře, dlouholetého kapitána daviscupové reprezentace a člena mezinárodní tenisové Síně slávy informoval národní svaz."
+title = "Dakar 2025: Vše o 46. ročníku nejslavnější rallye v Saudské Arábii"
+perex = "Začátek sportovního roku bude jako vždy patřit nejslavnější světové rallye. Dakar se pojede i v roce 2025 už pošesté za sebou v Saudské Arábii, a to v termínu od 3. do 17. ledna. Kompletní programový i výsledkový servis slavného motoristického závodu vám nabízí Sport.cz."
     
 
 create_model(model, vectorizer, data, retrain)
-# use_model(model, vectorizer, title, perex)
+use_model(model, vectorizer, title, perex)
