521f2a4c75d4a489d85bd48153b728e40a6bb929
Vojtech Lapunik <vojtechlapunik@gmail.com>
2024-12-09 09:33:43 +0100

    Update README.md

--- main.py
+++ main.py
@@ -2,24 +2,21 @@ from train import create_model
 from test import use_model
 
 # acc/f1
-available_models = ["bert", # 0.9879/0.9878("Seznam/dist-mpnet-paracrawl-cs-en") 
-                    "native_bayes", # tfidf: 0.98/0.98, count: 0.98/0.98, hashing: 0.98/0.98 
-                    "logistic_regression", # tfidf: 0.97/0.97  , count: 0.98/0.98, hashing: 0.97/0.96
-                    "decision_tree", # tfidf:0.93/0.93, count: 0.94/0.93, hashing: 0.93/0.93
-                    "mlp", # tfidf: 0.98/0.98, count: 0.98/0.98, hashing: 0./0.
-                    "random_forest", # tfidf: 0.96/0.95, count: 0.96/0.95, hashing: 0.95/0.95
-                    "kneighbors", # tfidf: 0.97/0.97, count: 0.77/0.76, hashing: 0.96/0.96
-                    "svm"]  # tfidf: 0.98/0.98, count: 0.98/0.98, hashing: 0./0.
+available_models = ["bert", 
+                    "native_bayes", 
+                    "logistic_regression", 
+                    "decision_tree", 
+                    "mlp", 
+                    "random_forest",
+                    "kneighbors", 
+                    "svm"] 
 
+# only if model is not bert
 available_preprocesors = ["count", 
                          "tfidf", 
                          "hashing",]
 
-available_data = ["sportoclanky",
-                  "first100",
-                  "first1k",
-                  "first10k",
-                  "first10"]
+available_data = ["sportoclanky"]
 
 model = "bert"
 preprocesor = "count"
@@ -30,5 +27,5 @@ title = "Pohár konstruktérů vyhrává McLaren, pro vítězství si v Abú Zab
 perex = "McLaren si zásluhou Landa Norrise dojel v Abú Zabí pro vítězství v letošním Poháru konstruktérů formule 1. Norris vyrážel z pole position a první místo udržel po dobu trvání celého závodu. Naopak jeho týmový kolega Oscar Piastri se po kolizi v prvním kole propadl a dojel desátý. Druhý dojel Carlos Sainz a třetí Charles Leclerc, ale Ferrari to na zisk týmového titulu nestačilo."
 
 if __name__ == '__main__':  
-    # create_model(model, preprocesor, data, retrain)
+    create_model(model, preprocesor, data, retrain)
     use_model(model, preprocesor, title, perex)
